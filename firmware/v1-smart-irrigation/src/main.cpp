c61a517785105860c6548f66923e802b2cb67a26
Breanna-J <kalebandbre@gmail.com>
2026-07-08 17:28:35 -0500

    Wrote the first code to read soil moisture.

--- firmware/v1-smart-irrigation/src/main.cpp
+++ firmware/v1-smart-irrigation/src/main.cpp
@@ -1,17 +1,24 @@
 #include <Arduino.h>
 
+const int SOIL_MOISTURE_SENSOR_PIN = 34; // ESP32 pin connected to the soil moisture sensor
+
 void setup() {
   Serial.begin(115200);
   delay(1000); // Wait for serial monitor to open
 
   Serial.println();
-  Serial.println("Project Eden V1 initialized"); //the project is started
+  Serial.println("Project Eden V1"); //the project is started
+  Serial.println("Sensor test initialized");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("System is running..."); // the project is working
+  int soilMoistureValue = analogRead(SOIL_MOISTURE_SENSOR_PIN); // Read the soil moisture sensor value
+
+  Serial.print("Soil Moisture Value: ");
+  Serial.println(soilMoistureValue); // the sensor value is printed to the serial monitor
+  
   delay(1000); // Delay for 1 second
 
 }
